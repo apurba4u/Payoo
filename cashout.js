0ebbc49eb7cf484860bb3c358ad1db8fe982225b
apurba4u <apurba185loser@gmail.com>
2026-02-18 20:05:47 +0600

    Cashout section Done

---
+++ cashout.js
@@ -0,0 +1,37 @@
+document.querySelector('#cashout-btn').addEventListener('click', () => {
+  // console.log('Working');
+  // 1 - get the agent number and validate
+  const cashoutNumberInput = document.querySelector("#cashout-number")
+  const cashoutNumber = cashoutNumberInput.value
+  if (cashoutNumber.length != 11) {
+    alert('Invalid Agent Number')
+    return
+  }
+  console.log(cashoutNumber);
+  // 2 - get the amount , validate, convert to Number
+  const cashoutAmountInput = document.querySelector("#cashout-amount")
+  const cashoutAmount = cashoutAmountInput.value
+  console.log(cashoutAmount);
+  // 3 - get the Current Balance , validate, convert to Number
+  const balanceElement = document.querySelector("#balance")
+  const balance = balanceElement.textContent
+  // 4 - calculate new Balance
+  const newBalance = Number(balance) - Number(cashoutAmount)
+  if (newBalance < 0) {
+    alert('Invalid Amount')
+    return
+  }
+  console.log(newBalance);
+  // 5 - Get the pin and verify
+  const cashoutPinInput = document.querySelector("#cashout-pin")
+  const pin = cashoutPinInput.value
+  if (pin == '4433') {
+    // 5 :: true >> show an alert set new balance
+    alert('Cashout Successfully')
+    balanceElement.textContent = newBalance
+  } else {
+    // 5 :: false >> show an error alert > return
+    alert('Invalid Pin')
+    return
+  }
+})
